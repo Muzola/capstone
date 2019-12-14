703354e9a32b4055adc51e88ce405a65290584c4
Muzola <amazzolaphoto@gmail.com>
2019-12-14 15:30:26 +0000

    trying to fix heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     password: ENV['GMAIL_PASSWORD']
   }
 
-  config.action_mailer.default_url_options = { host: 'localgost:3030' }
+  config.action_mailer.default_url_options = { host: 'https://capstone-anthony-m.herokuapp.com/' }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
